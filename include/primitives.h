331d89de59ce0bd795203d9e783d21ae3b8a3354
SoleSensei <dimargfd@gmail.com>
2018-04-27 13:35:36 +0300

    branch without opengl

--- include/primitives.h
+++ include/primitives.h
@@ -1,51 +1,32 @@
 #ifndef H_PRIMITIVES
 #define H_PRIMITIVES
 
-#include "common.h"
-#include "ShaderProgram.h"
+#include <iostream>
+#include <vector>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
+using glm::vec3;
 using std::cerr;
+using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
-
-class Box{
-    GLuint VBOvertices, VBOindices;
-
-    void Init(const float size);
-    
-public:
-    GLuint vao;
-
-    Box(const float size = 1){
-        Init(size);
-    };
-
-    void Draw(
-        const ShaderProgram& shader, const float4x4& projection,
-        const float4x4& view, const float4& shift = float4()
-    );
+struct Box
+{
+    vec3 boxMin, boxMax;
+    vec3 color;
+    Box(const vec3 vmin, const vec3 vmax, const vec3 col): boxMin(vmin), boxMax(vmax), color(col){}
 
 };
 
-class Plane{
-    GLuint VBOvertices,VBOindices;
-
-    void Init(const float size);
-    
-public:
-    GLuint vao;
-
-    Plane(const float size = 1){
-        Init(size);
-    };
-
-    void Draw(
-        const ShaderProgram& shader, const float4x4& projection, 
-        const float4x4& view, const float4& shift = float4()
-    );
-
+struct Plane
+{
+    vec3 lbot, rtop;
+    vec3 color;
+    Plane(const vec3 vlb, const vec3 vrt, const vec3 col): lbot(vlb), rtop(vrt), color(col){}
 };
+
 
 #endif
--- shaders/prim_fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 out_color;
-
-in vec3 TexCoords;
-
-void main()
-{    
-    out_color = vec4(0.2f, 0.5f, 0.5f, 1.0f);
-}
